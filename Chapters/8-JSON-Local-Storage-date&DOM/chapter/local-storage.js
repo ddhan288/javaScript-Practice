b7967fae3dd3c06ad71d54448a5cba86891ce355
ddhan288 <dev.deepakdhan@gmail.com>
2024-11-27 13:46:34 +0530

    added local-storage

---
+++ Chapters/8-JSON-Local-Storage-date&DOM/chapter/local-storage.js
@@ -0,0 +1,14 @@
+// 1.Persistent data storage in the browser.
+// 2 setltem: Stores data as key-value pairs.
+// 3 Only strings can be stored. getltem: Retrieves data 4 4 based on key.
+// 5 Other Methods: localStorage.clear(), removeltem().
+// 6. Do not store sensitive information. Viewable in storage console.
+
+localStorage.setItem("Name", "Deepak Dhan");
+localStorage.setItem("price", "53");
+
+console.log(localStorage.getItem("Name"));
+
+localStorage.set("product");
+
+
